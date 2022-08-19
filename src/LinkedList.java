fb264efbcff458b970f48b30feffb9f4aadd655e
aoshinbm <aoshinbm28797@gmail.com>
2022-08-19 22:32:02 +0530

    linked list commit

---
+++ src/LinkedList.java
@@ -0,0 +1,42 @@
+public class LinkedList<T> {
+
+    Node<T> head;
+    Node<T> tail;
+
+    public void push(T data){
+        Node<T> newNode= new Node(data);
+        if(head==null){
+            newNode=head;
+            head=tail;
+        }
+        else{
+            newNode.next= head;
+            head=newNode;
+        }
+    }
+
+    public void add(T data) {
+        Node<T> newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        }else{
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+
+    public void show(){
+        if(head==null) {
+            System.out.println("Linked list is empty");
+        }
+        else{
+            Node<T> temp=head;
+            while(head!=null){
+                System.out.println(" "+temp.data);
+                temp=temp.next;
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/UC1.java
@@ -0,0 +1,11 @@
+public class UC1 {
+    public static void main(String[] args) {
+
+        LinkedList<Integer> list1=new LinkedList();
+        list1.push(70);
+        list1.push(30);
+        list1.push(56);
+        list1.show();
+        list1.show();
+    }
+}
